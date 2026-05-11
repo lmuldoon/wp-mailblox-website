611938ded0adb0d02d523c4140bf68be8c156667
Luke Muldoon <LukeMuldoon@core-marketing.co.uk>
2026-05-11 17:38:07 +0100

    Update

--- template-parts/header.php
+++ template-parts/header.php
@@ -20,7 +20,7 @@ global $meta;
 	$IS_LIVE = preg_match($re, $_SERVER['SERVER_NAME']);
 	?>
 	<?php if ($IS_LIVE) { ?>
-		<script src="https://checkout.freemius.com/js/v1/" async></script>
+		<!-- <script src="https://checkout.freemius.com/js/v1/" async></script> -->
 		<script src="https://cdn.cookiehub.eu/c2/a7951233.js"></script>
 		<script type="text/javascript">
 			window.dataLayer = window.dataLayer || [];
